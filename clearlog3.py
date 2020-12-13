51c8aef76b98f173a8eeb79f4d4a52e14bb9b08f
Gulsum Atici <gulsumatici@gmail.com>
2020-12-13 21:10:30 +0300

    Create clearlog3.py

---
+++ clearlog3.py
@@ -0,0 +1,32 @@
+class  LogParser(object):
+    
+    def __init__(self,logfile):
+        self.logfile = logfile
+        self.final_store = []
+        self.content = []
+    
+    def _clear_log(self):
+        """
+        This method extracts time and ip information from raw log file
+        and store the clear log in final_store list.
+        """
+        try:
+            with  open(self.logfile,'r') as  file:
+                self.content = file.readlines()
+                for  line in self.content:
+                    info_wout_time = line.split('"',1)[1]
+                    self.final_store.append(info_wout_time)
+                return self.final_store
+        except:
+            print("Input file format is not proper for parsing")
+    
+    
+    def __str__(self):
+        text = '\n'.join(self._clear_log())
+        return text
+    
+ 
+
+if  __name__=='__main__':
+    log = LogParser('webserverlog2.txt')
+    print(log)
